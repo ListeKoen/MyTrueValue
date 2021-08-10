49e5fcee545eb17599afc91be44093324cae8978
Sebastian <sebhein17@gmail.com>
2021-08-10 13:56:13 +0200

    Hamburger menu implemented
    Needs a bit of tweaking, css wise

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+/* Hamburger menu toggle open/close */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+/* Hamburger menu, close when clicking link */
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
 /* Cookie Container */
 
 const cookieContainer = document.querySelector(".cookieContainer");
